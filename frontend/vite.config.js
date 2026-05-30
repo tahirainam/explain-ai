e30359b9eba8f2033d2291df3c276019a1808a87
tahirainam <cattblackwhite@gmail.com>
2026-05-30 07:06:48 -0700

    chore: configure for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
-    },
-  },
+  build: {
+    outDir: "dist",
+  }
 });
